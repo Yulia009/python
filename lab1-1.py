121e501d9ae2b92546c1c2f24f083296f84a3030
Yulia009 <144910098+Yulia009@users.noreply.github.com>
2023-09-14 12:03:54 +0300

    Update lab1.py

--- lab1-1.py
+++ lab1-1.py
@@ -1,3 +1,4 @@
+#1
 # 2*a + b  a > b
 # -2 a == b 
 # (a-5) /  b   a < b 
@@ -13,3 +14,25 @@ if a < b:
     ans = (a-5) / b
 
 print("Результат обчислення виразу", ans)
+#2
+# лінія 20 * кожна 3-я = 
+ans = ''
+for i in range(1,21):
+    s = '*'
+    if i % 3 == 0:
+        s = '='
+    ans = ans + s
+
+print(ans)
+#3
+# піраміда
+# 5
+# 4 5
+# 3 4 5
+# 2 3 4 5
+# 1 2 3 4 5 
+size = int(input("Введіть розмір піраміди = "))
+for i in range(0,size):
+    for j in range(size-i,size+1):
+        print(j, end =" ")
+    print()
